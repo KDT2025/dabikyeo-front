95bfc1a8834ab826fa04e592a855962f7b08e5f5
hongppa324 <hongppa324@gmail.com>
2025-08-18 15:12:17 +0900

    misc: 안 쓰는 코드 제거

--- providers.tsx
+++ providers.tsx
@@ -1,22 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import React from 'react';
-import { useColorScheme } from './src/hooks/useColorScheme.web';
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import React from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const [loaded] = useFonts({
+    SpaceMono: require("./src/assets/fonts/SpaceMono-Regular.ttf"),
+  });
 
-    const colorScheme = useColorScheme();
-      const [loaded] = useFonts({
-        SpaceMono: require('./src/assets/fonts/SpaceMono-Regular.ttf'),
-      });
-    
-      if (!loaded) {
-        // Async font loading only occurs in development.
-        return null;
-      }
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      {children}
-    </ThemeProvider>
-  );
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+  return <ThemeProvider value={DefaultTheme}>{children}</ThemeProvider>;
 }
